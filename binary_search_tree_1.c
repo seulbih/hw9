f9603e9c3c0ed3c395719eecc8cde2cc7e07a695
seulbih <tmfq1123@naver.com>
2021-05-04 20:04:06 +0900

    ㅁ ㅓㄴ가,, 된 것 같ㄸ ㅏ....

--- binary_search_tree_1.c
+++ binary_search_tree_1.c
@@ -28,7 +28,7 @@ int deleteLeafNode(Node* head, int key);  /* delete the leaf node for the key */
 Node* searchRecursive(Node* ptr, int key);  /* search the node for the key */
 Node* searchIterative(Node* head, int key);  /* search the node for the key */
 int freeBST(Node* head); /* free all memories allocated to the tree */
-
+void isEmpty(Node* ptr);
 
 /* you may add your own defined functions if necessary */
 
@@ -98,12 +98,15 @@ int main()
 			break;
 
 		case 'i': case 'I':
+			isEmpty(head->left);
 			inorderTraversal(head->left);
 			break;
 		case 'p': case 'P':
+			isEmpty(head->left);
 			preorderTraversal(head->left);
 			break;
 		case 't': case 'T':
+			isEmpty(head->left);
 			postorderTraversal(head->left);
 			break;
 		default:
@@ -130,32 +133,44 @@ int initializeBST(Node** h) {
 	return 1;
 }
 
-
+void isEmpty(Node* ptr){
+	if(ptr==NULL){
+		printf("tree is empty!\n");
+		return ;
+	}
+}
 void inorderTraversal(Node* ptr) //root노드 데려옴 //순환
 {/*중위순회 : 부모->왼->오*/
-	if(ptr!=NULL){
-	inorderTraversal(ptr->left);
-	printf("%3d", ptr->key);
-	inorderTraversal(ptr->right);
+
+	if(ptr!=NULL){ //순환형 중위순회
+	inorderTraversal(ptr->left); //왼쪽 방문
+	printf("%3d", ptr->key); //키값 출력
+	inorderTraversal(ptr->right); //오른쪽방문
 	}
+	else
+		return ;
 
 }
 
 void preorderTraversal(Node* ptr)
 {
-	if(ptr!=NULL){
-		printf("%3d", ptr->key);
-		preorderTraversal(ptr->left);
-		preorderTraversal(ptr->right);
+
+	if(ptr!=NULL){ //순환형 전위순회
+		printf("%3d", ptr->key); //ptr값 먼저 출력
+		preorderTraversal(ptr->left); //왼쪽방문
+		preorderTraversal(ptr->right); //오른쪽 방문
 	}
+	else
+		return ;
 }
 
 void postorderTraversal(Node* ptr)
 {
-	if(ptr!=NULL){
-		postorderTraversal(ptr->left);
-		postorderTraversal(ptr->right);
-		printf("%3d", ptr->key);
+
+	if(ptr!=NULL){ //순환형 후위순회
+		postorderTraversal(ptr->left);//왼쪽방문
+		postorderTraversal(ptr->right);//오른쪽 방문
+		printf("%3d", ptr->key); //키값출력
 	}
 }
 
@@ -173,10 +188,14 @@ int insert(Node* head, int key)
 	if(head->left==NULL)//초기상태인 경우, root노드가 없을 때
 	{
 		head->left=node;//헤드노드에 연결하려 root노드 생성
-		return 0;
+		return 1;
 	}
 
 	while(1){
+		if(temp->key==key){
+			printf("같은 값안됨!!!!");
+			return 0;
+		}
 		if(key<temp->key){ //key값이 root노드보다 작은 경우 > 왼쪽으로
 				if(temp->left == NULL){ //왼쪽 서브트리가 존재하지 않는 경우
 					temp->left = node; //노드 삽입
@@ -193,7 +212,6 @@ int insert(Node* head, int key)
 				else//오른쪽 서브트리가 존재하는 경우
 					temp=temp->right; //오른쪽 서브트리가 더이상 존재하지 않을 때까지 반복
 					}
-
 	}
 
 	return 0;
@@ -202,61 +220,46 @@ int insert(Node* head, int key)
 
 int deleteLeafNode(Node* head, int key)
 {
+	if(head->left==NULL){ //빈 트리일 경우
+			printf("tree is empty!\n");
+			return 0;
+		}
+
 	Node* temp = (Node*)malloc(sizeof(Node)); //임시노드 생성
 	Node* p = (Node*)malloc(sizeof(Node)); //임시노드
-	Node* del = (Node*)malloc(sizeof(Node)); //임시노드 생성
 
-	if (head->left==NULL){
-		printf("tree is empty!!!");
+	temp=head->left; //루트노드
+	p=NULL; //부모노드
+
+	while((temp!=NULL)&&(temp->key!=key)){ //노드 위치 탐색
+		p=temp;
+		if(key<temp->key) //찾는 값이 루트노드보다 작은 경우
+			temp=temp->left; //왼쪽트리로 옮겨 재탐색
+		else
+			temp=temp->right; //큰경우에는 오른쪽트리로 옮겨 재탐색
+	}
+	if(temp==NULL){ //key를 찾지 못한 경우
+		printf("%d is not in tree\n", key);
 		return 0;
 	}
 
-	temp=head->left; //root노드
-	if(temp->key>key){
-		temp->left=deleteLeafNode(temp->left, key);
-	}
-	else if (temp->key<key){
-		temp->right=deleteLeafNode(temp->right, key);
+	//key를 찾은 경우
+	if((temp->left==NULL)&&(temp->right==NULL)){ //leafnode인지 검사
+		if (p !=NULL){ //리프노드인 경우, 부모노드가 있는 경우
+			if(p->left==temp)//key가 왼쪽 노드인 경우
+				p->left=NULL;//부모노드의 왼쪽을 NULL로
+			else
+				p->right=NULL;//오른쪽 노드인 경우 오른쪽링크를 NULL로
+		}
+		else
+			head->left==NULL; //루트노드뿐인 트리, 루트노드=단말노드이므로 head->left를 NULL로
+		free(temp);
 	}
 	else{
-		if(temp->left==NULL){
-			p=temp->right;
-			free(temp);
-			return p;
-		}
-		else {
-			p=temp->left;
-			free(temp);
-			return temp;
-		}
+		printf("%d is not leafNode\n", key);
+		return 0;
 	}
 
-
-	/*while(temp->key !=key){ //key값을 찾을 때 까지 반복
-		p=temp; //부모노드 저장
-		if(key<temp->key){ //root노드보다 작을 때, 왼쪽으로 temp옮김
-			temp=temp->left;
-		}
-		else{ //root노드보다 클 때, 오른쪽으로 temp옮김
-			temp=temp->right;
-		}
-		if(temp=NULL) {//key값 없는 경우
-			printf("key is not in treeeee");
-			return 0; //그냥 종료
-		}
-	} //key값 찾으면 반복문 탈출 -> key=temp->key
-
-	if(temp->left==NULL&&temp->right==NULL){ //temp가 leafnode인지 판별
-		if(p->left!=NULL){ //부모의 왼쪽 노드가 NULL이 아니면
-			if(temp->key==p->left->key){//부모의 왼쪽노드가 찾는 값이면
-				p->left=NULL; //부모의 왼쪽노드 NULL로 설정
-				return 1;
-			}
-		}
-		p->right=NULL; //오른쪽 노드가 존재하고 NULL이 아니면 찾는 값이 오른쪽노드, NULL로설정해줌
-		free(temp); //키값 메모리 해제
-	}
-*/
 	return 0;
 }
 
@@ -302,74 +305,17 @@ Node* searchIterative(Node* head, int key)
 
 }
 
-//다시
+
 int freeBST(Node* head)
 {
-	Node* ptr = head->left; //부모노드
-	Node* lptr = NULL; //왼쪽트리
-	Node* rptr = NULL; //오른쪽트리
 
-	while(ptr!=NULL){
-		lptr=ptr->left; //자식노드 지정
-		rptr=ptr->right;
-		free(ptr); //부모노드 메모리 해제
-		if(lptr==NULL&&rptr==NULL){ //자식노드가 없으면 반복문 끝냄
-			if(head->left->right==NULL){ //헤드노드ㅇ
-			break;
-			}
-			else{
-				ptr=head->left->right;
-			}
-		}
-		else if(lptr==NULL){ //왼쪽자식이 없으면 오른쪽자식을 부모노드로 두고 반복
-			ptr=rptr;}
-		else if (rptr==NULL){//오른쪽자식이 없으면 왼쪽자식을 부모노드로 두고 반복
-			ptr=lptr;
-		}
-		else if (lptr!=NULL && rptr!=NULL){
-			ptr=lptr;
-			/*freeBST(lptr);
-			freeBST(rptr);*/
-		}
-		else{
-			if(lptr->right==NULL&&lptr->right==NULL){//왼쪽오른쪽자식 둘 다 있을 경우 리프노드인지 판별하여 메모리 해제
-				free(lptr);
-				ptr=rptr; //오른쪽자식을 부모노드로 다시 반복 (왼쪽에 노드 없음)
-			}
-			if(rptr->right==NULL&&rptr->right==NULL){ //리프노드아니면 처음부터 반복
-				free(rptr);
-				if(lptr!=NULL){ //왼쪽자식 메모리 해제 안된경우(리프노드 아닌경우)
-					ptr=lptr; //왼쪽자식을 부모노드로 반복 (오른쪽에 노드 없음)
-				}
-			}
-
-		}
-
+	Node* ptr = head->left;//루트노드
+	if(ptr!=NULL){//후위순위를 이용한 메모리 해제
+	postorderTraversal(ptr->left);
+	postorderTraversal(ptr->left);
+	free(ptr);
 	}
-
-	free(head); //헤드노드 메모리 해제
-
-
-
-	/*while(ptr != NULL) { //트리가 존재할 때, 왼쪽트리부터 순회
-		lptr=ptr;
-		lptr=lptr->left;
-		rptr=ptr->right;
-		free(ptr);
-			}
-
-		while(rptr != NULL) { //트리가 존재할 때, 왼쪽트리부터 순회
-			p = rptr;
-			rptr = rptr->right;
-			free(p);
-			}
-		free(head); //head노드 해제*/
-
-	/*if(ptr!=NULL){
-		freeBST(ptr->left);
-		freeBST(ptr->right);
-		free(ptr);
-	}*/
+	free(head); //헤드노드까지 해제해줌
 	return 0;
 }
 
